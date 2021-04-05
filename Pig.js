418fb190965663acf6af827e690d9327292af534
Deepika Navani <deepnavani@gmail.com>
2021-04-05 12:29:38 +0530

    Add files via upload

--- Pig.js
+++ Pig.js
@@ -2,7 +2,7 @@ class Pig extends BaseClass {
   constructor(x, y){
     super(x,y,50,50);
     this.image = loadImage("sprites/enemy.png");
- //this.Visiblity=255;
+ this.Visiblity=255;
   }
 display()
 {
@@ -13,12 +13,12 @@ if((this.body.speed)<3)
 {
 World.remove(world,this.body)  
 
-//push();
-//this.Visiblity=this.Visiblity-5;
-//tint(255,this.Visiblity)
+push();
+this.Visiblity=this.Visiblity-5;
+tint(255,this.Visiblity)
 
-//do nothing
-//image(this.image,this.body.position.x,this.body.position.y,50,50)
-//pop();
+
+image(this.image,this.body.position.x,this.body.position.y,50,50)
+pop();
 }}
 };
